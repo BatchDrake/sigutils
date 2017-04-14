8588147c2bbe1abdca614d45d285fcf9c8b75eb7
Gonzalo J. Carracedo <BatchDrake@gmail.com>
2017-04-14 16:27:14 +0200

    Add space after log message prefix

--- sigutils/lib.c
+++ sigutils/lib.c
@@ -58,7 +58,7 @@ su_log_func_default(void *private, const struct sigutils_log_message *msg)
   if (*cr)
     fprintf(
         stderr,
-        "[%c] %s:%d:",
+        "[%c] %s:%d: ",
         su_log_severity_to_char(msg->severity),
         msg->function,
         msg->line);
