8ce8ea85195b166ce33cb46a7bbe2d454a7c549b
Gonzalo J. Carracedo <BatchDrake@gmail.com>
2019-08-09 12:23:03 +0200

    Move NCQO login to inline functions in header files

--- sigutils/ncqo.c
+++ sigutils/ncqo.c
@@ -23,73 +23,10 @@
 
 #define SU_LOG_DOMAIN "ncqo"
 
+#include "log.h"
 #include "ncqo.h"
 #include "sampling.h"
 #include "config.h"
-#include "log.h"
-
-#if defined(_SU_SINGLE_PRECISION) && HAVE_VOLK
-#  define SU_USE_VOLK
-#  define SU_VOLK_CALL_STRIDE_BITS 5
-#  define SU_VOLK_CALL_STRIDE      (1 << SU_VOLK_CALL_STRIDE_BITS)
-#  define SU_VOLK_CALL_STRIDE_MASK (SU_VOLK_CALL_STRIDE - 1)
-#  include <volk/volk.h>
-#endif
-
-SUINLINE void
-__su_ncqo_step(su_ncqo_t *ncqo)
-{
-  ncqo->phi += ncqo->omega;
-
-  if (ncqo->phi >= 2 * PI)
-    ncqo->phi -= 2 * PI;
-  else if (ncqo->phi < 0)
-    ncqo->phi += 2 * PI;
-}
-
-#ifdef SU_NCQO_USE_PRECALC_BUFFER
-SUINLINE void
-__su_ncqo_populate_precalc_buffer(su_ncqo_t *ncqo)
-{
-  unsigned int i, p;
-
-  /* Precalculate phase buffer */
-  for (i = 0; i < SU_NCQO_PRECALC_BUFFER_LEN; ++i) {
-    ncqo->phi_buffer[i] = ncqo->phi;
-#  ifndef SU_USE_VOLK
-#    ifdef HAVE_SINCOS
-    SU_SINCOS(ncqo->phi, ncqo->sin_buffer + i, ncqo->cos_buffer + i);
-#    else
-    ncqo->sin_buffer[i] = SU_SIN(ncqo->phi);
-    ncqo->cos_buffer[i] = SU_COS(ncqo->phi);
-#    endif /* HAVE_SINCOS */
-#  else
-    if ((i & SU_VOLK_CALL_STRIDE_MASK) == SU_VOLK_CALL_STRIDE_MASK) {
-      p = i & ~SU_VOLK_CALL_STRIDE_MASK;
-      volk_32f_sin_32f(
-          ncqo->sin_buffer + p,
-          ncqo->phi_buffer + p,
-          SU_VOLK_CALL_STRIDE);
-      volk_32f_cos_32f(
-          ncqo->cos_buffer + p,
-          ncqo->phi_buffer + p,
-          SU_VOLK_CALL_STRIDE);
-    }
-#  endif /* SU_USE_VOLK */
-    __su_ncqo_step(ncqo);
-  }
-}
-
-SUINLINE void
-__su_ncqo_step_precalc(su_ncqo_t *ncqo)
-{
-  if (++ncqo->p == SU_NCQO_PRECALC_BUFFER_LEN) {
-    ncqo->p = 0;
-    __su_ncqo_populate_precalc_buffer(ncqo);
-  }
-}
-
-#endif /* SU_NCQO_USE_PRECALC_BUFFER */
 
 /* Expects: relative frequency */
 void
@@ -137,24 +74,6 @@ __su_ncqo_assert_sin(su_ncqo_t *ncqo)
 }
 
 void
-su_ncqo_step(su_ncqo_t *ncqo)
-{
-#ifdef SU_NCQO_USE_PRECALC_BUFFER
-  if (ncqo->pre_c) {
-    __su_ncqo_step_precalc(ncqo);
-  } else {
-#endif /* SU_NCQO_USE_PRECALC_BUFFER */
-    __su_ncqo_step(ncqo);
-
-    /* Sine & cosine values are now outdated */
-    ncqo->cos_updated = SU_FALSE;
-    ncqo->sin_updated = SU_FALSE;
-#ifdef SU_NCQO_USE_PRECALC_BUFFER
-  }
-#endif /* SU_NCQO_USE_PRECALC_BUFFER */
-}
-
-void
 su_ncqo_set_phase(su_ncqo_t *ncqo, SUFLOAT phi)
 {
 #ifdef SU_NCQO_USE_PRECALC_BUFFER
@@ -165,34 +84,6 @@ su_ncqo_set_phase(su_ncqo_t *ncqo, SUFLOAT phi)
 #endif /* SU_NCQO_USE_PRECALC_BUFFER */
 
   ncqo->phi = phi - 2 * PI * SU_FLOOR(phi / (2 * PI));
-}
-
-SUFLOAT
-su_ncqo_get_phase(su_ncqo_t *ncqo)
-{
-#ifdef SU_NCQO_USE_PRECALC_BUFFER
-  if (ncqo->pre_c)
-    return ncqo->phi_buffer[ncqo->p];
-#endif /* SU_NCQO_USE_PRECALC_BUFFER */
-
-  return ncqo->phi;
-}
-
-void
-su_ncqo_inc_phase(su_ncqo_t *ncqo, SUFLOAT delta)
-{
-#ifdef SU_NCQO_USE_PRECALC_BUFFER
-  if (ncqo->pre_c) {
-    SU_ERROR("Cannot increase phase on a fixed NCQO\n");
-    return;
-  }
-#endif /* SU_NCQO_USE_PRECALC_BUFFER */
-
-  ncqo->phi += delta;
-
-  if (ncqo->phi < 0 || ncqo->phi >= 2 * PI) {
-    ncqo->phi -= 2 * PI * SU_FLOOR(ncqo->phi / (2 * PI));
-  }
 }
 
 SUFLOAT
--- sigutils/ncqo.h
+++ sigutils/ncqo.h
@@ -61,6 +61,85 @@ su_phase_adjust(SUFLOAT phi)
   return phi - 2 * PI * SU_FLOOR(phi / (2 * PI));
 }
 
+SUINLINE SUFLOAT
+su_phase_adjust_one_cycle(SUFLOAT phi)
+{
+  if (phi > PI)
+    return phi - 2 * PI;
+
+  if (phi < - PI)
+    return phi + 2 * PI;
+
+  return phi;
+}
+
+SUINLINE void
+__su_ncqo_step(su_ncqo_t *ncqo)
+{
+  ncqo->phi += ncqo->omega;
+
+  if (ncqo->phi >= 2 * PI)
+    ncqo->phi -= 2 * PI;
+  else if (ncqo->phi < 0)
+    ncqo->phi += 2 * PI;
+}
+
+/* vvvvvvvvvvvvvvvvvvvvvvvvvvv VOLK HACKS BELOW vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
+#if defined(_SU_SINGLE_PRECISION) && HAVE_VOLK
+#  define SU_USE_VOLK
+#  define SU_VOLK_CALL_STRIDE_BITS 5
+#  define SU_VOLK_CALL_STRIDE      (1 << SU_VOLK_CALL_STRIDE_BITS)
+#  define SU_VOLK_CALL_STRIDE_MASK (SU_VOLK_CALL_STRIDE - 1)
+#  include <volk/volk.h>
+#endif
+
+#ifdef SU_NCQO_USE_PRECALC_BUFFER
+SUINLINE void
+__su_ncqo_populate_precalc_buffer(su_ncqo_t *ncqo)
+{
+  unsigned int i;
+#ifdef SU_USE_VOLK
+  unsigned int p;
+#endif /* SU_USE_VOLK */
+  /* Precalculate phase buffer */
+  for (i = 0; i < SU_NCQO_PRECALC_BUFFER_LEN; ++i) {
+    ncqo->phi_buffer[i] = ncqo->phi;
+#  ifndef SU_USE_VOLK
+#    ifdef HAVE_SINCOS
+    SU_SINCOS(ncqo->phi, ncqo->sin_buffer + i, ncqo->cos_buffer + i);
+#    else /* HAVE_SINCOS */
+    ncqo->sin_buffer[i] = SU_SIN(ncqo->phi);
+    ncqo->cos_buffer[i] = SU_COS(ncqo->phi);
+#    endif /* HAVE_SINCOS */
+#  else
+    if ((i & SU_VOLK_CALL_STRIDE_MASK) == SU_VOLK_CALL_STRIDE_MASK) {
+      p = i & ~SU_VOLK_CALL_STRIDE_MASK;
+      volk_32f_sin_32f(
+          ncqo->sin_buffer + p,
+          ncqo->phi_buffer + p,
+          SU_VOLK_CALL_STRIDE);
+      volk_32f_cos_32f(
+          ncqo->cos_buffer + p,
+          ncqo->phi_buffer + p,
+          SU_VOLK_CALL_STRIDE);
+    }
+#  endif /* SU_USE_VOLK */
+    __su_ncqo_step(ncqo);
+  }
+}
+
+SUINLINE void
+__su_ncqo_step_precalc(su_ncqo_t *ncqo)
+{
+  if (++ncqo->p == SU_NCQO_PRECALC_BUFFER_LEN) {
+    ncqo->p = 0;
+    __su_ncqo_populate_precalc_buffer(ncqo);
+  }
+}
+
+#endif /* SU_NCQO_USE_PRECALC_BUFFER */
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ VOLK HACKS ABOVE ^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
 /* NCQO constructor */
 void su_ncqo_init(su_ncqo_t *ncqo, SUFLOAT frel);
 
@@ -68,17 +147,59 @@ void su_ncqo_init(su_ncqo_t *ncqo, SUFLOAT frel);
 void su_ncqo_init_fixed(su_ncqo_t *ncqo, SUFLOAT fnor);
 
 /* Compute next step */
-void su_ncqo_step(su_ncqo_t *ncqo);
+SUINLINE void
+su_ncqo_step(su_ncqo_t *ncqo)
+{
+#ifdef SU_NCQO_USE_PRECALC_BUFFER
+  if (ncqo->pre_c) {
+    __su_ncqo_step_precalc(ncqo);
+  } else {
+#endif /* SU_NCQO_USE_PRECALC_BUFFER */
+    __su_ncqo_step(ncqo);
+
+    /* Sine & cosine values are now outdated */
+    ncqo->cos_updated = SU_FALSE;
+    ncqo->sin_updated = SU_FALSE;
+#ifdef SU_NCQO_USE_PRECALC_BUFFER
+  }
+#endif /* SU_NCQO_USE_PRECALC_BUFFER */
+}
+
 
 /* Force phase */
 void su_ncqo_set_phase(su_ncqo_t *ncqo, SUFLOAT phi);
 
 /* Get current phase */
-SUFLOAT su_ncqo_get_phase(su_ncqo_t *ncqo);
+SUINLINE SUFLOAT
+su_ncqo_get_phase(su_ncqo_t *ncqo)
+{
+#ifdef SU_NCQO_USE_PRECALC_BUFFER
+  if (ncqo->pre_c)
+    return ncqo->phi_buffer[ncqo->p];
+#endif /* SU_NCQO_USE_PRECALC_BUFFER */
+
+  return ncqo->phi;
+}
 
 /* Increment current phase */
-void su_ncqo_inc_phase(su_ncqo_t *ncqo, SUFLOAT delta);
+SUINLINE void
+su_ncqo_inc_phase(su_ncqo_t *ncqo, SUFLOAT delta)
+{
+#ifdef SU_NCQO_USE_PRECALC_BUFFER
+  if (ncqo->pre_c) {
+#  ifdef SU_LOG_DOMAIN
+    SU_ERROR("Cannot increase phase on a fixed NCQO\n");
+#  endif /* SU_LOG_DOMAIN */
+    return;
+  }
+#endif /* SU_NCQO_USE_PRECALC_BUFFER */
 
+  ncqo->phi += delta;
+
+  if (ncqo->phi < 0 || ncqo->phi >= 2 * PI) {
+    ncqo->phi -= 2 * PI * SU_FLOOR(ncqo->phi / (2 * PI));
+  }
+}
 /* Get in-phase component */
 SUFLOAT su_ncqo_get_i(su_ncqo_t *ncqo);
 
