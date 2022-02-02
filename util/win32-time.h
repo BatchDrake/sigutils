d4ef4e7a1823d7bcd382cb0a6117736cb221df40
Gonzalo José Carracedo Carballal <BatchDrake@gmail.com>
2022-02-02 17:06:13 +0100

    Prevent redefinition of timer functions

--- util/win32-time.h
+++ util/win32-time.h
@@ -32,30 +32,38 @@ extern "C" {
 
 /* timeradd, timersub, timercmp were borrowed from the
    Standard GNU C Library */
-# define timercmp(a, b, CMP)                                      \
-  (((a)->tv_sec == (b)->tv_sec)                                   \
-   ? ((a)->tv_usec CMP (b)->tv_usec)                              \
-   : ((a)->tv_sec CMP (b)->tv_sec))
-# define timeradd(a, b, result)                                   \
-  do {                                                            \
-    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                 \
-    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;              \
-    if ((result)->tv_usec >= 1000000)                             \
-      {                                                           \
-  ++(result)->tv_sec;                                             \
-  (result)->tv_usec -= 1000000;                                   \
-      }                                                           \
-  } while (0)
-# define timersub(a, b, result)                                   \
-  do {                                                            \
-    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                 \
-    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;              \
-    if ((result)->tv_usec < 0) {                                  \
-      --(result)->tv_sec;                                         \
-      (result)->tv_usec += 1000000;                               \
-    }                                                             \
-  } while (0)
-              
+# ifndef timercmp
+#   define timercmp(a, b, CMP)                                      \
+    (((a)->tv_sec == (b)->tv_sec)                                   \
+      ? ((a)->tv_usec CMP (b)->tv_usec)                             \
+    : ((a)->tv_sec CMP (b)->tv_sec))
+# endif /* timercmp */
+
+# ifndef timeradd
+#   define timeradd(a, b, result)                                   \
+    do {                                                            \
+      (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                 \
+      (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;              \
+      if ((result)->tv_usec >= 1000000)                             \
+        {                                                           \
+    ++(result)->tv_sec;                                             \
+    (result)->tv_usec -= 1000000;                                   \
+        }                                                           \
+    } while (0)
+# endif /* timeradd */
+
+# ifndef timersub
+#   define timersub(a, b, result)                                   \
+    do {                                                            \
+      (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                 \
+      (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;              \
+      if ((result)->tv_usec < 0) {                                  \
+        --(result)->tv_sec;                                         \
+        (result)->tv_usec += 1000000;                               \
+      }                                                             \
+    } while (0)
+#endif /* timersub */
+          
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
