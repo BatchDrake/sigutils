e7dd843d90b375d672a73f4e53358b87b80b8b08
Gonzalo José Carracedo Carballal <BatchDrake@gmail.com>
2022-02-02 17:02:25 +0100

    Use macros for timersub functions

--- util/win32-time.c
+++ util/win32-time.c
@@ -15,32 +15,3 @@
 
 #include "win32-time.h"
 
-/**
- * timersub(3) from <sys/time.h>
- */
-void timersub(const struct timeval *a, const struct timeval *b,
-							struct timeval *res)
-{
-	res->tv_sec = a->tv_sec - b->tv_sec;
-	res->tv_usec = a->tv_usec - b->tv_usec;
-	if (res->tv_usec < 0)
-	{
-		res->tv_usec += 1000000;
-		res->tv_sec--;
-	}
-}
-
-/**
- * timeradd(3) from <sys/time.h>
- */
-void timeradd(const struct timeval *a, const struct timeval *b,
-							struct timeval *res)
-{
-	res->tv_sec = a->tv_sec + b->tv_sec;
-	res->tv_usec = a->tv_usec + b->tv_usec;
-	if (res->tv_usec > 1000000)
-	{
-		res->tv_usec -= 1000000;
-		res->tv_sec++;
-	}
-}
--- util/win32-time.h
+++ util/win32-time.h
@@ -1,12 +1,16 @@
 /*
+  
   Copyright (C) 2022 Ángel Ruiz Fernández
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as
   published by the Free Software Foundation, version 3.
+  
   This program is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Lesser General Public License for more details.
+  
   You should have received a copy of the GNU Lesser General Public
   License along with this program.  If not, see
   <http://www.gnu.org/licenses/>
@@ -26,14 +30,34 @@
 extern "C" {
 #endif /* __cplusplus */
 
-void timeradd(const struct timeval *a, const struct timeval *b,
-							struct timeval *res);
-
-void timersub(const struct timeval *a, const struct timeval *b,
-							struct timeval *res);
-							
+/* timeradd, timersub, timercmp were borrowed from the
+   Standard GNU C Library */
+# define timercmp(a, b, CMP)                                      \
+  (((a)->tv_sec == (b)->tv_sec)                                   \
+   ? ((a)->tv_usec CMP (b)->tv_usec)                              \
+   : ((a)->tv_sec CMP (b)->tv_sec))
+# define timeradd(a, b, result)                                   \
+  do {                                                            \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                 \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;              \
+    if ((result)->tv_usec >= 1000000)                             \
+      {                                                           \
+  ++(result)->tv_sec;                                             \
+  (result)->tv_usec -= 1000000;                                   \
+      }                                                           \
+  } while (0)
+# define timersub(a, b, result)                                   \
+  do {                                                            \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                 \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;              \
+    if ((result)->tv_usec < 0) {                                  \
+      --(result)->tv_sec;                                         \
+      (result)->tv_usec += 1000000;                               \
+    }                                                             \
+  } while (0)
+              
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-				
+        
 #endif /* _UTIL_TIME_H */
